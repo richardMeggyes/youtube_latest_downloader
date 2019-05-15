7679b52820d24282798b91231d2eaac7d46057b9
Readdeo <readdeo91@gmail.com>
2019-05-15 09:49:43 +0200

    Added description and info downloading to the command

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ sync_latest_video_path = "/media/readdeo/maindata/SYNCH/LatestYoutube/"
 #             "CoachRedPill":"https://www.youtube.com/channel/UC5tEELgWBfKbA9fVPRzBzPQ"}
 
 # channel name, storage path, channel link
-youtube_dl_link = 'youtube-dl -f \'bestvideo[height<=480]+bestaudio/best[height<=480]\' --download-archive downloaded_{}.txt --no-post-overwrites --restrict-filenames -ciw -o "{}%(upload_date)s_%(title)s_%()s_%(display_id)s.%(ext)s" -v {}'
+youtube_dl_link = 'youtube-dl -f \'bestvideo[height<=480]+bestaudio/best[height<=480]\' --write-description --write-info-json --download-archive downloaded_{}.txt --no-post-overwrites --restrict-filenames -ciw -o "{}%(upload_date)s_%(title)s_%()s_%(display_id)s.%(ext)s" -v {}'
 # number of videos to download, channel name, storage path, channel link
 youtube_dl_link_limited = 'youtube-dl --playlist-items 1-{} -f \'bestvideo[height<=480]+bestaudio/best[height<=480]\' --download-archive downloaded_{}.txt --no-post-overwrites --restrict-filenames -ciw -o "{}%(upload_date)s_%(title)s_%()s_%(display_id)s.%(ext)s" -v {}'
